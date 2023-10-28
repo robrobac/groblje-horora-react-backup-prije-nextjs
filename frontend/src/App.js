843da157886d49183a04bfa0f36e3fb4c9c8aadb
Roberto <roberto.vukomanovic@gmail.com>
2023-10-28 13:37:05 +0200

    react-router-dom installed and starter routes set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Recenzije from './pages/Recenzije';
+import RecenzijaSingle from './pages/RecenzijaSingle';
+
 function App() {
     return (
         <div className="App">
-            app
+            <BrowserRouter>
+                <div className="pages">
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/recenzije' element={<Recenzije />}>
+                            <Route path=':id' element={<RecenzijaSingle />} />
+                        </Route>
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
